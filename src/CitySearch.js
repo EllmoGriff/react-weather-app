19de42ebda547b11d43ba32de02104a79c0e4eaa
EllmoGriff <114524662+EllmoGriff@users.noreply.github.com>
2022-12-09 15:06:15 +0000

    sort of unbroken it

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -9,9 +9,31 @@ export default function CitySearch() {
 	let [description, setDescription] = useState("");
 	let [humidity, setHumidity] = useState("");
 	let [wind, setWind] = useState("");
-	let [icon, setIcon] = useState("");
 	let [realFeel, setRealFeel] = useState("");
 
+	
+let details = (<div className="MainTempDetails">
+			  <div className="mood" id="description">
+			    {description}
+			  </div>
+			  <div className="container text-center deets">
+			    <div className="row">
+			      <div className="col-sm-4">
+			        <span className="feel"> </span>
+			        <span id="degree">{temperature}°</span>
+			      </div>
+			      <div className="col-sm-4">
+			        <span className="wind">Wind </span>
+			        <span id="mph">{wind} mph</span>
+			      </div>
+			      <div className="col-sm-4">
+			        <span className="humid">Humidity </span>
+			        <span id="percent">{humidity}%</span>
+			      </div>
+			    </div>
+			  </div>
+			</div>);
+			
 	function showTemperature(response) {
 		setTemperature(response.data.main.temp);
 		setDescription(response.data.weather[0].description);
@@ -19,7 +41,6 @@ export default function CitySearch() {
 		setWind(response.data.wind.speed);
 		setIcon(response.data.weather[0].icon);
 		// setRealFeel(response.data.)
-
 		console.log(response.data);
 	}
 
@@ -28,68 +49,17 @@ export default function CitySearch() {
 		if (city.length > 0) {
 			let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6d68aadfacdd4f5163bc273049a0cf2d&units=Metric`;
 			axios.get(url).then(showTemperature);
-			//   setMessage{
-			//     <div className="MainTempDetails">
-			//   <div className="mood" id="description">
-			//     {description}
-			//   </div>
-			//   <div className="container text-center deets">
-			//     <div className="row">
-			//       <div className="col-sm-4">
-			//         <span className="feel">{Real feel} </span>
-			//         <span id="degree">{temperature}°</span>
-			//       </div>
-			//       <div className="col-sm-4">
-			//         <span className="wind">Wind </span>
-			//         <span id="mph">{wind} mph</span>
-			//       </div>
-			//       <div className="col-sm-4">
-			//         <span className="humid">Humidity </span>
-			//         <span id="percent">{humidity}%</span>
-			//       </div>
-			//     </div>
-			//   </div>
-			// </div>
-			//   }
-			//   setMessage(
-			//     <ul className="showTemperature">
-			//       <li>{city}</li>
-			//       <li>Temperature: {Math.round(temperature)}°C</li>
-			//       <li>Description: {description}</li>
-			//       <li>Humidity:{humidity}%</li>
-			//       <li>Wind:{Math.round(wind)}km/h </li>
-			//       <li>
-			//         {" "}
-			//         <img
-			//           src={`http://openweathermap.org/img/w/${icon}.png`}
-			//           alt="Weather icons"
-			//         />
-			//       </li>
-			//     </ul>
-			//   );
-			// } else {
-			//   setMessage(`Please Enter a City`);
-			// }
+			  setMessage(details);
+			  }
 		}
+
 		function onGeoLoation() {}
+
 		function updateCity(event) {
 			setCity(event.target.value);
 		}
-		return (
-			<div className="searchEngine">
-				<form onSubmit={handleSubmit}>
-					{" "}
-					<input
-						type="search"
-						onChange={updateCity}
-						placeholder="Where to?"
-					/>
-					<input type="submit" value="search" id="searchButton" />
-				</form>
-				<h2>{message}</h2>
-			</div>
-		);
-	}
+
+	
 	return (
 		<section>
 			<div className="citySearch">
