91d766609c55b6a87f0e2fa099b94669c30ec56c
EllmoGriff <114524662+EllmoGriff@users.noreply.github.com>
2022-12-09 14:52:45 +0000

    Broken it

--- src/MainTempDetails.js
+++ src/MainTempDetails.js
@@ -2,27 +2,27 @@ import React from "react";
 import "./MainTempDetails.css";
 
 export default function MainTempDetails() {
-  return (
-    <div className="MainTempDetails">
-      <div className="mood" id="description">
-        Partly Cloudy
-      </div>
-      <div className="container text-center deets">
-        <div className="row">
-          <div className="col-sm-4">
-            <span className="feel">Real feel </span>
-            <span id="degree">°</span>
-          </div>
-          <div className="col-sm-4">
-            <span className="wind">Wind </span>
-            <span id="mph"> mph</span>
-          </div>
-          <div className="col-sm-4">
-            <span className="humid">Humidity </span>
-            <span id="percent">%</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="MainTempDetails">
+			<div className="mood" id="description">
+				Partly Cloudy
+			</div>
+			<div className="container text-center deets">
+				<div className="row">
+					<div className="col-sm-4">
+						<span className="feel">Real feel </span>
+						<span id="degree">°</span>
+					</div>
+					<div className="col-sm-4">
+						<span className="wind">Wind </span>
+						<span id="mph"> mph</span>
+					</div>
+					<div className="col-sm-4">
+						<span className="humid">Humidity </span>
+						<span id="percent">%</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
